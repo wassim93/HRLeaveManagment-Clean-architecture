13bea55e2b5c204421898e1f66596310e2256f8b
wassim93x <b.wassim1993@gmail.com>
2024-05-28 23:34:27 +0200

    implementing mediator pattern

---
+++ HR.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HR.Application.DTOs;
+using HR.Application.Features.LeaveTypes.Requests;
+using HR.Application.Persistance.Contracts;
+using MediatR;
+
+namespace HR.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeListRequestHandler : IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveTypeListRequestHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            var leaveTypes = await _leaveTypeRepository.GetAll();
+            return _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+        }
+    }
+}
---
+++ HR.Application/Features/LeaveTypes/Requests/GetLeaveTypeListRequest.cs
@@ -0,0 +1,9 @@
+﻿using HR.Application.DTOs;
+using MediatR;
+
+namespace HR.Application.Features.LeaveTypes.Requests
+{
+    public class GetLeaveTypeListRequest : IRequest<List<LeaveTypeDto>>
+    {
+    }
+}
