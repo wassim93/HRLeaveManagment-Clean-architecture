de5d527c38bdba337ed78e171e70532d9d4257eb
wassim93x <b.wassim1993@gmail.com>
2024-06-03 21:11:19 +0200

    adding persistance layer & dbcontext

---
+++ HR.Persistance/LeaveManagmentDBContext.cs
@@ -0,0 +1,38 @@
+﻿using HR.Domain;
+using HR.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.Persistance
+{
+    public class LeaveManagmentDBContext : DbContext
+    {
+        public LeaveManagmentDBContext(DbContextOptions<LeaveManagmentDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagmentDBContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+
+
+    }
+}
