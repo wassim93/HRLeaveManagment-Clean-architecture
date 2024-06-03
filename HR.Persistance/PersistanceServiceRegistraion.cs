e96b3e202e9790deb12e95ada2fb2edbd6c9eddd
wassim93x <b.wassim1993@gmail.com>
2024-06-03 21:45:03 +0200

    implement registartion service for persistance

---
+++ HR.Persistance/PersistanceServiceRegistraion.cs
@@ -0,0 +1,25 @@
+﻿using HR.Application.Persistance.Contracts;
+using HR.Persistance.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.Persistance
+{
+    public static class PersistanceServiceRegistraion
+    {
+        public static IServiceCollection ConfigurePersistanceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagmentDBContext>(cfg =>
+            cfg.UseSqlServer(configuration.GetConnectionString("ConnectionString")));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllocationRepositroy, LeaveAllocationRepository>();
+
+            return services;
+
+        }
+    }
+}
